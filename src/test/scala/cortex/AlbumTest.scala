59386e5b82b8f7e8287e5fb43a74eade4340d12d
alan <alanwu2002@hotmail.com>
2015-10-18 15:12:24 -0600

    added matching

--- src/test/scala/cortex/AlbumTest.scala
+++ src/test/scala/cortex/AlbumTest.scala
@@ -2,13 +2,11 @@ package cortex
 
 import org.scalatest.{ShouldMatchers, FunSpec}
 
-/**
- * Created by awu on 10/18/2015.
- */
 class AlbumTest extends FunSpec with ShouldMatchers{
   describe("An album"){
     it("can add an Artist object to the album") {
       val album = new Album("Thriller", 1981, new Artist("Michael", "Jackson"))
+      album.artist.firstName should be ("Michael")
     }
   }
 }
